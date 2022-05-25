193787a3a9b4ce4233d2eabef38727c44d0d1a11
Eterevskij Oleg <eterevskijoleg@gmail.com>
2022-05-25 21:26:47 +0300

    bug with slides ref

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, {useState} from 'react';
 import 'antd/dist/antd.css';
 import './App.css';
-import { Select } from 'antd';
 
 import StatisticsCards from './components/StatisticsCards';
 import Services from './components/Services';
 
-import { ReactComponent as Calendar } from './icons/Calendar.svg';
-import { ReactComponent as SelectIcon } from './icons/SelectIcon.svg';
 
 
-const { Option } = Select;
+
 
 function App() {
 
@@ -18,35 +15,9 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="statistics">
 
-          <div className="statistics__header">
-            <p className="statistics__header__title">Статистика</p>
-            <div className="statistics__header__period">
-              <Calendar className="statistics__header__period__icon" />
-
-              <Select className="statistics__header__period__select"
-                defaultValue="allTime"
-                suffixIcon={<SelectIcon />}>
-                <Option value="allTime">За все время</Option>
-                <Option value="option2">Вариант 2</Option>
-                <Option value="option3">Вариант 3</Option>
-
-              </Select>
-
-            </div>
-          </div>
-
-          <div className="statistics__wrapper">
-
-            <StatisticsCards />
-
-          </div>
-
-        </div>
-
-      
-        <Services />
+          <StatisticsCards />
+          <Services />
 
 
       </div>
--- src/components/StatisticsCards.jsx
+++ src/components/StatisticsCards.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row } from 'antd';
+import { Row, Select } from 'antd';
 
 import SmallCard from './SmallCard';
 
@@ -8,22 +8,50 @@ import { ReactComponent as Applications } from '../icons/Applications.svg';
 import { ReactComponent as Home } from '../icons/Home.svg';
 import { ReactComponent as CheckCircle } from '../icons/CheckCircle.svg';
 import { ReactComponent as DealStoryIcon } from '../icons/DealStoryIcon.svg';
+import { ReactComponent as Calendar } from '../icons/Calendar.svg';
+import { ReactComponent as SelectIcon } from '../icons/SelectIcon.svg';
+
+const { Option } = Select;
 
 const StatisticsCards = () => {
 
-    return (
-        <div className="statistics__smallCardWrapper">
-        <Row gutter={24}>
+  return (
+    <div className="statistics">
 
-          <SmallCard title='Мои клиенты' value={78} difference={1} Icon={Clients} buttonText={'Добавить клиента'} />
-          <SmallCard title='Мои заявки' value={42} difference={1} Icon={Applications} buttonText={'Создать заявку'} />
-          <SmallCard title='Ипотечные заявки' value={7} Icon={Home} buttonText={'Создать заявку'} />
-          <SmallCard title='Сделок проведено' value={13} Icon={CheckCircle} buttonText={'История сделок'} rightButtonIcon ButtonIcon={DealStoryIcon} />
+      <div className="statistics__header">
+        <p className="statistics__header__title">Статистика</p>
+        <div className="statistics__header__period">
+          <Calendar className="statistics__header__period__icon" />
 
-        </Row>
+          <Select className="statistics__header__period__select"
+            defaultValue="allTime"
+            suffixIcon={<SelectIcon />}>
+            <Option value="allTime">За все время</Option>
+            <Option value="option2">Вариант 2</Option>
+            <Option value="option3">Вариант 3</Option>
+
+          </Select>
+
+        </div>
       </div>
 
-    )
+      <div className="statistics__wrapper">
+        <div className="statistics__smallCardWrapper">
+          <Row gutter={24}>
+
+            <SmallCard title='Мои клиенты' value={78} difference={1} Icon={Clients} buttonText={'Добавить клиента'} />
+            <SmallCard title='Мои заявки' value={42} difference={1} Icon={Applications} buttonText={'Создать заявку'} />
+            <SmallCard title='Ипотечные заявки' value={7} Icon={Home} buttonText={'Создать заявку'} />
+            <SmallCard title='Сделок проведено' value={13} Icon={CheckCircle} buttonText={'История сделок'} rightButtonIcon ButtonIcon={DealStoryIcon} />
+
+          </Row>
+        </div>
+
+      </div>
+
+    </div>
+
+  )
 }
 
 export default StatisticsCards; 
